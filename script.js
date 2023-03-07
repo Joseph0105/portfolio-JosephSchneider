82c10f0c03907feb1b9d43e89ee710d40c297850
joseph_dev <joseph.schneider0105@gmail.com>
2023-03-07 18:58:47 -0500

    avancement sur le portfolio, problème sur l'affichage des images

--- script.js
+++ script.js
@@ -7,7 +7,11 @@
 })();
 
 function getProjects() {
-  return fetch(`https://api.github.com/users/Joseph0105/repos?per_page=100`)
+  return fetch(`https://api.github.com/users/Joseph0105/repos?per_page=100`, {
+    headers: {
+      Authorization: "Bearer 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",
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
       const repositories = data;
@@ -26,18 +30,19 @@ function displayProjects(project) {
   const projectCard = document.createElement("div");
   projectCard.classList.add("projectCard");
 
+  const projectLink = document.createElement("a");
+  projectLink.classList.add("projectCard-link");
+  projectLink.href = `https://joseph0105.github.io/${project.name}/`;
+
+  const projectImage = document.createElement("img");
+  projectImage.classList.add(".project-img");
+  projectImage.src = `https://raw.githubusercontent.com/Joseph0105/${project.name}/main/img/miniature.png`;
+  projectLink.appendChild(projectImage);
+
   const projectName = document.createElement("h2");
   projectName.textContent = project.name;
-  projectCard.appendChild(projectName);
+  projectLink.appendChild(projectName);
 
-  const productDescription = document.createElement("p");
-  productDescription.textContent = project.description;
-  projectCard.appendChild(productDescription);
-
-  const projectLink = document.createElement("a");
-  projectLink.textContent = "Voir le projet";
-  projectLink.href = `https://joseph0105.github.io/${project.name}/`;
   projectCard.appendChild(projectLink);
-
   projectsDisplay.appendChild(projectCard);
 }
