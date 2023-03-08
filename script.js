2243c226f7925a5a88fae1e0446add937a775220
joseph_dev <joseph.schneider0105@gmail.com>
2023-03-08 10:16:39 -0500

    réglage des flexbox du portfolio

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-(async function () {
-  const projects = await getProjects();
-  console.log(projects);
-  for (project of projects) {
-    displayProjects(project);
-  }
-})();
+// (async function () {
+//   const projects = await getProjects();
+//   console.log(projects);
+//   for (project of projects) {
+//     displayProjects(project);
+//   }
+// })();
 
-function getProjects() {
-  return fetch(`https://api.github.com/users/Joseph0105/repos?per_page=100`, {
-    headers: {
-      Authorization: "Bearer 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",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const repositories = data;
-      const repositoriesDisplay = document.querySelector(".projects-display");
+// function getProjects() {
+//   return fetch(`https://api.github.com/users/Joseph0105/repos?per_page=100`, {
+//     headers: {
+//       Authorization: "Bearer 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",
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const repositories = data;
+//       const repositoriesDisplay = document.querySelector(".projects-display");
 
-      return repositories;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+//       return repositories;
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
 
-function displayProjects(project) {
-  const projectsDisplay = document.querySelector(".projects-display");
+// function displayProjects(project) {
+//   const projectsDisplay = document.querySelector(".projects-display");
 
-  const projectCard = document.createElement("div");
-  projectCard.classList.add("projectCard");
+//   const projectCard = document.createElement("div");
+//   projectCard.classList.add("projectCard");
 
-  const projectLink = document.createElement("a");
-  projectLink.classList.add("projectCard-link");
-  projectLink.href = `https://joseph0105.github.io/${project.name}/`;
+//   const projectLink = document.createElement("a");
+//   projectLink.classList.add("projectCard-link");
+//   projectLink.href = `https://joseph0105.github.io/${project.name}/`;
 
-  const projectImage = document.createElement("img");
-  projectImage.classList.add(".project-img");
-  projectImage.src = `https://raw.githubusercontent.com/Joseph0105/${project.name}/main/img/miniature.png`;
-  projectLink.appendChild(projectImage);
+//   const projectImage = document.createElement("img");
+//   projectImage.classList.add(".project-img");
+//   projectImage.src = `https://raw.githubusercontent.com/Joseph0105/${project.name}/main/img/miniature.png`;
+//   projectLink.appendChild(projectImage);
 
-  const projectName = document.createElement("h2");
-  projectName.textContent = project.name;
-  projectLink.appendChild(projectName);
+//   const projectName = document.createElement("h2");
+//   projectName.textContent = project.name;
+//   projectLink.appendChild(projectName);
 
-  projectCard.appendChild(projectLink);
-  projectsDisplay.appendChild(projectCard);
-}
+//   projectCard.appendChild(projectLink);
+//   projectsDisplay.appendChild(projectCard);
+// }
