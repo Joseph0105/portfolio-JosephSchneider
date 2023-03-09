f6db52b135e528378b542458f4d7d9e02a1fcf6a
joseph_dev <joseph.schneider0105@gmail.com>
2023-03-09 16:59:19 -0500

    stylisation du bouton pour le cv avec la mixin

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 function getProjects() {
   return fetch(`https://api.github.com/users/Joseph0105/repos?per_page=100`, {
     headers: {
-      Authorization: "Bearer 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",
+      Authorization: "Bearer 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",
     },
   })
     .then((response) => response.json())
@@ -132,7 +132,7 @@ function displayOverlay(project) {
   // Affichage des langages
   fetch(`https://api.github.com/repos/Joseph0105/${project.name}/languages`, {
     headers: {
-      Authorization: "Bearer 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",
+      Authorization: "Bearer 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",
     },
   })
     .then((response) => response.json())
