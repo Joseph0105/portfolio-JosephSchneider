3897ab309c339b04f8decbd9a21a488ed0011811
joseph_dev <joseph.schneider0105@gmail.com>
2023-03-15 09:54:15 -0400

    ajout du responsive pour l'affichage du cv

--- scripts/cv.js
+++ scripts/cv.js
@@ -1,21 +1,32 @@
-// Ouvrir le CV
-const cvLink = document.querySelector(".CV-link");
+function isMobileDevice() {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
 
-const cvModal = document.querySelector(".cv-modal");
+document.addEventListener("DOMContentLoaded", function () {
+  const cvLink = document.querySelector(".CV-link");
+  const cvIframe = document.querySelector(".cv-iframe");
+  const cvModal = document.querySelector(".cv-modal");
+  const cvContainer = document.querySelector(".cv-container");
 
-cvLink.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  cvModal.style.display = "block";
-});
-// Fermer le cv
-
-const cvContainer = document.querySelector(".cv-container");
-
-cvModal.addEventListener("click", function (e) {
-  if (e.target === cvContainer) {
-    return;
+  if (isMobileDevice()) {
+    cvLink.href = cvIframe.getAttribute("data-src");
+    cvLink.setAttribute("download", "");
   } else {
-    cvModal.style.display = "none";
+    cvLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      cvIframe.src = cvIframe.getAttribute("data-src");
+      cvModal.style.display = "block";
+    });
+
+    cvModal.addEventListener("click", function (e) {
+      if (e.target === cvContainer) {
+        return;
+      } else {
+        cvModal.style.display = "none";
+      }
+    });
   }
 });
