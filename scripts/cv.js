05b039083f8b75362bb872671751aa0b684a1044
joseph_dev <joseph.schneider0105@gmail.com>
2023-03-16 10:16:14 -0400

    m

--- scripts/cv.js
+++ scripts/cv.js
@@ -5,39 +5,36 @@ function isMobileDevice() {
   );
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  // e.preventDefault();
-  const cvLink = document.querySelector('.CV-link');
-  const cvIframe = document.querySelector('.cv-iframe');
-  const cvModal = document.querySelector('.cv-modal');
-  const cvContainer = document.querySelector('.cv-container');
-  const cvClose = document.querySelector('.cv-close');
-  function closeCV(cvClose) {
-    cvClose.addEventListener('click', function (e) {
-      cvModal.style.display = 'none';
-    });
-  }
-  if (isMobileDevice()) {
-    cvLink.addEventListener('click', function (e) {
-      e.preventDefault();
-      cvIframe.src = cvIframe.getAttribute('data-src');
-      cvModal.style.display = 'block';
-      closeCV();
-    });
-  } else {
-    cvLink.addEventListener('click', function (e) {
-      e.preventDefault();
-      cvIframe.src = cvIframe.getAttribute('data-src');
-      cvModal.style.display = 'block';
-      closeCV();
-    });
+const cvLink = document.querySelector('.CV-link');
+const cvIframe = document.querySelector('.cv-iframe');
+const cvModal = document.querySelector('.cv-modal');
+const cvContainer = document.querySelector('.cv-container');
+const cvClose = document.querySelector('.cv-close');
+function closeCV() {
+  cvClose.addEventListener('click', function (e) {
+    cvModal.style.display = 'none';
+  });
+}
+if (isMobileDevice()) {
+  cvLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    cvIframe.src = cvIframe.getAttribute('data-src');
+    cvModal.style.display = 'block';
+    closeCV();
+  });
+} else {
+  cvLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    cvIframe.src = cvIframe.getAttribute('data-src');
+    cvModal.style.display = 'block';
+  });
 
-    cvModal.addEventListener('click', function (e) {
-      if (e.target === cvContainer) {
-        return;
-      } else {
-        cvModal.style.display = 'none';
-      }
-    });
-  }
-});
+  cvModal.addEventListener('click', function (e) {
+    if (e.target === cvContainer) {
+      closeCV();
+      return;
+    } else {
+      cvModal.style.display = 'none';
+    }
+  });
+}
